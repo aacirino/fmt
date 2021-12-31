5583a723d53459011edbadb23d0f3b02eff02910
Ângelo Andrade Cirino <aacirino@gmail.com>
2021-12-30 22:04:25 -0300

    Added mold to the cmake configuration

--- init.sh
+++ init.sh
@@ -2,4 +2,8 @@ cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
     -DCMAKE_CXX_COMPILER:STRING=clang++ \
     -DCMAKE_CXX_FLAGS:STRING="-I$(brew --prefix)/opt/llvm/include -I$(brew --prefix)/opt/llvm/include/c++/v1 -I/usr/local/include -march=native" \
+    -DCMAKE_EXE_LINKER_FLAGS:STRING="$LDFLAGS -fuse-ld=mold" \
+    -DCMAKE_MODULE_LINKER_FLAGS:STRING="$LDFLAGS -fuse-ld=mold" \
+    -DCMAKE_SHARED_LINKER_FLAGS:STRING="$LDFLAGS -fuse-ld=mold" \
+    -DCMAKE_LINKER:FILEPATH=$(which mold) \
     -DCMAKE_CXX_STANDARD=20
